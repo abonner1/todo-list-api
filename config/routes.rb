e06aea9e01845ea1c3aa440accbc68ffd3ba11ef
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-13 19:14:00 -0400

    Change scope to namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '/api' do
+  namespace :api do
     resources :users, except: [:index, :new, :edit] do
       resources :todo_lists, shallow: true do
         resources :todos, only: [:create, :update, :destroy]
