c2e481edfb9118dffceb4d9bba3eeff8c19f5044
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-13 19:12:47 -0400

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/api' do
-    resources :users, only: [:create, :show, :update, :destroy] do
+    resources :users, except: [:index, :new, :edit] do
       resources :todo_lists, shallow: true do
         resources :todos, only: [:create, :update, :destroy]
       end
