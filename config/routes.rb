e96e93dc4607acfda681fdaa9d10191b03e43489
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-13 16:49:58 -0400

    Create routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :users, only: [:create, :show, :update, :destroy] do
+      resources :todo_lists, shallow: true do
+        resources :todos, only: [:create, :update, :destroy]
+      end
+    end
+  end
 end
