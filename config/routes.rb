1d93928bf5715ababd3e3c9a50edf58c048c3b90
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 14:07:18 -0400

    Add Knock config and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   namespace :api do
-    resources :users, except: [:index, :new, :edit] do
-      resources :todo_lists, shallow: true do
-        resources :todos, only: [:create, :update, :destroy]
-      end
+    post 'user_token' => 'user_token#create'
+    resources :todo_lists, shallow: true do
+      resources :todos, only: [:index, :create, :update, :destroy]
     end
   end
 end
+
+# resources :users, except: [:index, :new, :edit] do
+# end
