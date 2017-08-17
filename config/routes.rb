1e74022ee2fac7f042bfdedf4480358fa09ce429
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 15:24:19 -0400

    Allow only create user path.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     post 'user_token' => 'user_token#create'
+    resources :users, only: [:create]
     resources :todo_lists, shallow: true do
       resources :todos, only: [:index, :create, :update, :destroy]
     end
   end
 end
-
-# resources :users, except: [:index, :new, :edit] do
-# end
