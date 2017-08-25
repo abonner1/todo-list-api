6d0b077c9b7002698338da292e3e12e2714f1665
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-25 12:27:51 -0400

    Remove show and update routes from TodoLists.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     post 'user_token' => 'user_token#create'
     resources :users, only: [:create]
-    resources :todo_lists, shallow: true do
+    resources :todo_lists, only: [:index, :create, :destroy], shallow: true do
       resources :todos, only: [:index, :create, :update, :destroy]
     end
   end
